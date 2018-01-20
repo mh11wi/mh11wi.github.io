d677e6d707bfec9d71e7f1b6db07c55f7953a1f7
mh11wi <madeleine.hill1@mohawkcollege.ca>
2018-01-20 16:54:04 -0500

    Cross-browser styling fixes

--- js/site.js
+++ js/site.js
@@ -1,27 +1,4 @@
 $(document).ready(function() {
-
-    function hasTouch() {
-	return 'ontouchstart' in document.documentElement
-            || navigator.maxTouchPoints > 0
-            || navigator.msMaxTouchPoints > 0;
-    }
-
-    if (hasTouch()) { // remove all :hover stylesheets
-	try { // prevent exception on browsers not supporting DOM styleSheets properly
-            for (var si in document.styleSheets) {
-		var styleSheet = document.styleSheets[si];
-		if (!styleSheet.rules) continue;
-
-		for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                    if (!styleSheet.rules[ri].selectorText) continue;
-
-                    if (styleSheet.rules[ri].selectorText.match(':hover')) {
-			styleSheet.deleteRule(ri);
-                    }
-		}
-            }
-	} catch (ex) {}
-    }
     
     //Page loaded to particular hash
     if (window.location.hash) {
@@ -50,6 +27,7 @@ $(document).ready(function() {
 		scrollTop: target.offset().top - h + 1
 	    }, 1000);
         }
+	target.focus();
     });
 
     //Collapse menu on scroll
